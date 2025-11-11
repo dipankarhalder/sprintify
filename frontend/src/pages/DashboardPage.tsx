bd6d75016f34661c20f9fb72af26f70ecde2e714
Dipankar Halder <dipankarhalder@Dipankars-MacBook-Pro.local>
2025-11-11 07:39:58 +0530

    mostly completed important part of sidebar

--- frontend/src/pages/DashboardPage.tsx
+++ frontend/src/pages/DashboardPage.tsx
@@ -1,5 +1,23 @@
 import React from 'react'
 
 export const DashboardPage = () => {
-  return <div>DashboardPage</div>
+  return (
+    <div className="app_box_full">
+      <div className="app-box" style={{ background: '#0057ff' }}></div>
+      <div
+        className="app-box"
+        style={{ background: 'rgba(0, 87, 255, 0.2)' }}
+      ></div>
+      <div className="app-box" style={{ background: '#028901' }}></div>
+      <div className="app-box"></div>
+      <div className="app-box"></div>
+      <div className="app-box"></div>
+      <div className="app-box"></div>
+      <div className="app-box"></div>
+      <div className="app-box"></div>
+      <div className="app-box"></div>
+      <div className="app-box"></div>
+      <div className="app-box"></div>
+    </div>
+  )
 }
