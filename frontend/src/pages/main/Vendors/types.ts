f32da92b974ccfef91e41df3e69dcd1ef5a8571b
Dipankar Halder <dipankarhalder@Dipankars-MacBook-Pro.local>
2025-11-16 04:58:30 +0530

    added data with reusabel component

---
+++ frontend/src/pages/main/Vendors/types.ts
@@ -0,0 +1,9 @@
+export type VendorTypes = {
+  id?: string
+  name: string
+  contactPerson: string
+  email: string
+  phone: string
+  category: string
+  status: string
+}
---
+++ frontend/src/shared/Breadcrumb/index.tsx
@@ -0,0 +1,24 @@
+/** node modules */
+import { Fragment } from 'react'
+import { Link } from 'react-router-dom'
+
+/** types */
+import type { BreadcrumbProps } from './types'
+
+export const Breadcrumb = ({ items }: BreadcrumbProps) => {
+  return (
+    <div className="app_top_page_information">
+      <div className="app_top_breadcumb">
+        {items.map((item, index) => (
+          <Fragment key={index}>
+            <Link to={item.to}>
+              {item.icon}
+              {item.label}
+            </Link>
+            {index < items.length - 1 && <span>/</span>}
+          </Fragment>
+        ))}
+      </div>
+    </div>
+  )
+}
